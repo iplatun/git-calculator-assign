a2b3f40673b4f9c5ff5b3f999502697eb3889f03
private information <5893707+Coocoocoocoooo@users.noreply.github.com>
2021-04-29 10:10:33 -0500

    First JS attempt

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//Calculate Tip
+function calculateTip() {
+    let total = document.getElementById("total").value;           
+    let rating = document.getElementById("rating").value;  
+    let guests = document.getElementById("guests").value;
+
+}
